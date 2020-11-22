c9a5d6d376020e769053dc8b00fcea21f8a8497e
noemiepierart <noemie.pierart@gmail.com>
2020-11-22 21:46:58 +0100

    Updated settings for Heroku

---
+++ power4/static/power4/brouillon.js
@@ -0,0 +1,32 @@
+
+// // Play 1 : make yellow player play : 
+// const clicked_dot1 = document.getElementById("");
+// clicked_dot1.classList.add(`${color1}`);
+// clicked_dot1.classList.add("slideInDown"); 
+// clicked_dot1.classList.remove("empty");
+// message.innerText = `${player2_username}, it's your turn !`;
+
+// // Play 2 : make red player play : 
+// const clicked_dot2 = document.getElementById("");
+// clicked_dot2.classList.add(`${color2}`);
+// clicked_dot2.classList.add("slideInDown"); 
+// clicked_dot2.classList.remove("empty");
+// message.innerText = `${player1_username}, it's your turn !`;
+
+// // Play 2 : make red player play : 
+// const clicked_dot3 = document.getElementById("");
+// clicked_dot3.classList.add(`${color1}`);
+// clicked_dot3.classList.add("slideInDown"); 
+// clicked_dot3.classList.remove("empty");
+// message.innerText = `${player2_username}, it's your turn !`;
+
+// // Make winning dots blink :
+// const winning_dots = [winning_dot0, winning_dot1, winning_dot2, winning_dot3]
+// const winning_dot0 = document.getElementById("");
+// const winning_dot1 = document.getElementById("");
+// const winning_dot2 = document.getElementById("");
+// const winning_dot3 = document.getElementById("");
+
+// winning_dots.forEach((dot) => {
+// 	dot.classList.add("winning-dot");
+// })
--- project5/settings.py
+++ project5/settings.py
@@ -24,11 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+#DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-export DJANGO_DEBUG=False
 
 # Application definition
 
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +121,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
